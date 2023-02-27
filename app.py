e3c0a335b2398336af974f3a3d2efe69dcf0f920
Syniron <66834451+kynetical@users.noreply.github.com>
2023-02-27 04:13:25 -0500

    remove unused new member feature and cleanup

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Load packages
 import discord
-#from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 import os
 
 # Credentials
-#load_dotenv('.env')
+# load_dotenv('.env')
 
 # Grant necessary intents to see reactions/member list
 intents = discord.Intents.default()
@@ -54,6 +55,7 @@ async def on_raw_reaction_add(payload):
         else:
             print("Role Not Found")
 
+
 # Remove role based on Emoji Name - - Emoji name and Role name MUST MATCH
 @client.event
 async def on_raw_reaction_remove(payload):
@@ -85,32 +87,7 @@ async def on_raw_reaction_remove(payload):
         else:
             print("Role Not Found")
 
-#send message to new users who join the server
-@client.event
-async def on_member_join(member):
-    embed = discord.Embed(title="Welcome to the 7th Cavalry Discord Server!", url="https://discord.com/channels/109869242148491264/437743413228732436/876589551022391396",
-                          description="We're glad to have you!", colour=15844367)
-    embed.set_author(name="CavBot",
-                     url="http://discord.gg/7cav",
-                     icon_url="https://wiki.7cav.us/images/3/36/UNK_Avatar_Mourning.png")
-    embed.set_thumbnail(
-        url="https://wiki.7cav.us/images/3/36/UNK_Avatar_Mourning.png")
-    embed.add_field(name="Welcome Channel",
-                    value="Please be sure to read our rules and information in the #welcome Channel.",
-                    inline=True)
-    embed.add_field(name="Recruitment",
-                    value="If you're interested in joining the cav visit #recruitment and one of our recruiters will be happy to help.",
-                    inline=True)
-    embed.add_field(name="Game Specific Channels",
-                    value="Visit #channel-roles to choose the roles for the games you're interested in.",
-                    inline=True)
-    embed.add_field(name="Other Clans and Communities",
-                    value="If you represent another clan or community say hello in #public-relations and our public relations staff will be in touch.",
-                    inline=True)
-    embed.set_footer(text="We hope you enjoy your stay, any questions at all feel free to ask in #public-lounge")
-    await member.send(embed=embed)
-    print("Sent Message to New User")
 
 # Bot Token
-token = client.run(os.getenv('BOT_TOKEN'))
+token = client.run(os.getenv("BOT_TOKEN"))
 client.run(token)
